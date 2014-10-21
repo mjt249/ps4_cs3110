c71495fea1436f75353a312eaafb6a0e6983a5ae
mjt249 <mjt249@cornell.edu>
2014-10-21 16:03:47 -0400

    merge error

--- interpreter/eval.ml
+++ interpreter/eval.ml
@@ -16,73 +16,25 @@ and environment = value ref Environment.environment
 (* Parses a datum into an expression. *)
 let rec read_expression (input : datum) : expression =
   match input with
-  | Nil -> failwith "nil"
   | Atom (Identifier id) when Identifier.is_valid_variable id ->
-     ExprVariable id
-  | Atom (Identifier id) -> failwith "is a keyword"
-  | Atom (Boolean bl) -> ExprSelfEvaluating (SEBoolean bl)
-  | Atom (Integer intgr) -> ExprSelfEvaluating (SEInteger intgr)
-  | Cons (first, second) ->
-     (*match first with
-       | Atom (Identifier id) when Identifier.is_valid_variable id ->
-          ExprVariable id
-       | Atom (Identifier id) -> when id = "quote" -> failwith "quote"
-       | Atom (Identifier id) -> when id = "if" -> failwith "if"
-       | Atom (Identifier id) -> when id = "lambda" -> failwith "lambda"
-       | Atom (Identifier id) -> when id = "set!" -> failwith "set!"
-       | Atom (Identifier id) -> when id = "let" -> failwith "let"
-       | Atom (Identifier id) -> when id = "let*" -> failwith "let*"
-       | Atom (Identifier id) -> when id = "letrec" -> failwith "letrec"
-       | _ -> failwith "no match" *)
+     failwith "Oh my God!  You are The Rower!"
+  | Atom (Identifier id) ->
      (* Above match case didn't succeed, so id is not a valid variable. *)
      failwith "I'm such a huge fan!"
-  | _ -> failwith "Everything you do is just amazing!"
+  | _ ->
+     failwith "Everything you do is just amazing!"
 
 (* Parses a datum into a toplevel input. toplevel = definition | expression. 
    so call read_expression and then if it fails, try definition. if that fails
    parsing failed.*) 
 let read_toplevel (input : datum) : toplevel =
-  ToplevelExpression (read_expression(input))
-  (*match input with
-  | _ -> failwith "Sing the Rowing Song!" *)
+  match input with
+  | _ -> failwith "Sing the Rowing Song!"
 
 (* This function returns an initial environment with any built-in
    bound variables. *)
 let rec initial_environment () : environment =
-  let func_car (single_cell: value) : datum = 
-    match single_cell) with 
-    | ValDatum (el1, el2) -> el1
-    | _ -> failwith "not a single con_cell" in
-  let func_cdr (single_cell: value) : datum =
-    match single_cell with
-    | ValDatum (el1, el2) -> el2
-    | ValDatum (el1) -> Nil
-    | _ -> failwith "not a single con_cell" in
-  let rec func_add (single_cell: value) : datum =
-    match (car single_cell) with
-    | ValDatum (Nil) -> 0
-    | ValDatum (SEInteger x) -> (x + (func_add (cdr single_cell)))
-    | _ -> failwith "must add integers" in
-  
-  let car = Identifier.variable_of_identifier(Identifier.identifier_of_string("car")) in
-  let cdr = Identifier.variable_of_identifier(Identifier.identifier_of_string("cdr")) in
-  let add = Identifier.variable_of_identifier(Identifier.identifier_of_string("+")) in
-  let mult = Identifier.variable_of_identifier(Identifier.identifier_of_string("*")) in
-  let is_eq = Identifier.variable_of_identifier(Identifier.identifier_of_string("equal?")) in
-  let evl = Identifier.variable_of_identifier(Identifier.identifier_of_string("eval")) in
-
-  let init_env = Environment.empty_environment in
-  Environment.addbinding init_env (Identifier.variable_of_identifier("course": Identifier), 3110)
-
-
-  (*use addbinding.
-  [(car, func_car ); (cdr, func_cdr); (add, ); (mult, ); (is_eq, ); (evl, eval) ] *)
-
-let self_eval_eval (se_expr: self_evaluating) : value =
-  match se_expr with
-  | SEInteger integer -> ValDatum (Atom (Integer integer))
-  | SEBoolean boolean -> ValDatum (Atom (Integer boolean))
-
+  failwith "You know!"
 
 (* Evaluates an expression down to a value in a given environment. *)
 (* You may want to add helper functions to make this function more
@@ -91,7 +43,7 @@ let self_eval_eval (se_expr: self_evaluating) : value =
    statement. *)
 and eval (expression : expression) (env : environment) : value =
   match expression with
-  | ExprSelfEvaluating se -> self_eval_eval
+  | ExprSelfEvaluating _
   | ExprVariable _        ->
      failwith "'Oh I sure love to row my boat with my...oar."
   | ExprQuote _           ->
