6b16605e3c05942d863cfde1f73de98383b71ce0
mjt249 <mjt249@cornell.edu>
2014-10-23 14:10:06 -0400

    work on let star

--- interpreter/eval.ml
+++ interpreter/eval.ml
@@ -16,7 +16,7 @@ and environment = value ref Environment.environment
 
 (* Parses a datum into an expression. *)
 let rec read_expression (input : datum) : expression =
-   let rec cons_to_expr_list (cons: datum) (acc: expression list) : expression list =
+  let rec cons_to_expr_list (cons: datum) (acc: expression list) : expression list =
     match cons with
     | Cons ( first, second ) -> cons_to_expr_list second ((read_expression first)::acc) 
     | Nil -> List.rev(acc)
@@ -28,6 +28,14 @@ let rec read_expression (input : datum) : expression =
         cons_to_var_list second ((Identifier.variable_of_identifier id)::acc) 
     | Nil -> List.rev(acc)
     | _ -> failwith "not a cons or not a var (to_var_list)" in 
+
+  let rec cons_to_let_binding_list (cons: datum) (acc: let_binding list): let_binding list =
+    match cons with
+    | Cons ( Atom (Identifier id), x) -> failwith "first"
+       (*cons_to_let_binding_list rest (((Identifier.variable_of_identifier id), (read_expression expr)):: acc)*)
+    | Nil -> List.rev(acc)
+    | _ -> failwith "not a valid binding list" in
+
   match input with
   | Atom (Identifier id) when Identifier.is_valid_variable id ->
 
@@ -49,6 +57,9 @@ let rec read_expression (input : datum) : expression =
                       (cons_to_expr_list arguments []))
   | Cons ( Atom (Identifier id), Cons ( Atom ( Identifier var), Cons (expr, Nil)) ) when (id = Identifier.identifier_of_string("set!")) ->
       ExprAssignment ( (Identifier.variable_of_identifier var), read_expression expr)
+  | Cons ( Atom (Identifier id), Cons (let_binds, expressions) ) when (id = Identifier.identifier_of_string("let*")) ->
+      ExprLetStar ((cons_to_let_binding_list let_binds []), (cons_to_expr_list expressions []))
+
 (*
        | Atom (Identifier id) -> when id = "let" -> failwith "let"
        | Atom (Identifier id) -> when id = "let*" -> failwith "let*"
@@ -62,13 +73,11 @@ let rec read_expression (input : datum) : expression =
    parsing failed.*) 
 let read_toplevel (input : datum) : toplevel =
   match input with
-<<<<<<< HEAD
-  | _ -> failwith "Sing the Rowing Song!"
-=======
+
   | Cons (Atom (Identifier id), Cons( Atom (Identifier var), Cons( expr, Nil ))) when (id = (Identifier.identifier_of_string("define")))-> 
       ToplevelDefinition ((Identifier.variable_of_identifier var), read_expression expr)
   | _ -> ToplevelExpression (read_expression input )
->>>>>>> e4d58ed79cf347591ca1ef247f63c63fbb4bea9c
+
 
 (* This function returns an initial environment with any built-in
    bound variables. *)
@@ -105,6 +114,7 @@ let rec initial_environment () : environment =
     match two_vals with
     | e1::e2::[] -> ValDatum (Atom (Boolean (e1 = e2)))
     | _ -> failwith "Invalid arguments to eqaul?." in
+
 
   let func_eval (one_val: value list) (env: environment) : value =
     match one_val with
@@ -205,15 +215,45 @@ and eval (expression : expression) (env : environment) : value =
       ValDatum Nil
     else 
       failwith "variable must already be bound" in
-    
-  let proc_lambda_eval (variables: variable list) (env: environment) (expressions: expression list)
-   (arguments: expression list) : value =
-    let rec proc_lambda_eval_helper (expr_list: expression list) (temp_env: environment) 
+
+
+  let rec proc_lambda_eval_helper (expr_list: expression list) (temp_env: environment) 
      (acc:value): value=
       match expr_list with
       | hd::tl -> proc_lambda_eval_helper tl temp_env (eval hd temp_env)
-      | _ -> acc in
+      | _ -> acc in 
+
+  let proc_lambda_eval (variables: variable list) (env: environment) (expressions: expression list)
+   (arguments: expression list) : value =
     proc_lambda_eval_helper expressions (add_temp_bindings env variables arguments) (ValDatum Nil) in
+
+  let let_star (bind_lst: let_binding list) (expr: expression list) : value =
+     let rec separate_bindings (bind_l: let_binding list) (acc: variable list * expression list) =
+         match bind_l with
+         | (var,exp)::tl -> separate_bindings tl (var::(fst acc), exp::(snd acc))
+         | [] -> acc
+        
+       in
+    let not_reversed_sep = (separate_bindings bind_lst ([],[])) in
+    let sep_binds = ((List.rev(fst not_reversed_sep)), (List.rev(snd not_reversed_sep))) in
+    let temp_env = add_temp_bindings env (fst sep_binds) (snd sep_binds) in
+
+     (* let rec var_lst_maker (vlst: variable list) (new_vlists: variable list * expression list )=
+         match vlst with
+          |hd::tl -> if (List.exists (fun x -> (hd = x)) vlst) then var_lst_maker tl new_vlists
+             else let v = !(Environment.get_binding temp_env hd) in
+             var_lst_maker tl ((hd::(fst new_vlists)), (v::(snd new_vlists)))
+          | [] -> new_vlists 
+        in 
+    
+     let vars_and_args = var_lst_maker (fst sep_binds) ([],[]) in
+     let rev_vars_and_args = ((List.rev(fst vars_and_args)), (List.rev(snd vars_and_args))) in *)
+     (* proc_lambda_eval (fst rev_vars_and_args) (Environment.combine env temp_env) expr (snd rev_vars_and_args)  *)
+     proc_lambda_eval_helper expr (Environment.combine_environments env temp_env) (ValDatum Nil)
+   in
+
+  
+
 
   match expression with
 
@@ -229,8 +269,8 @@ and eval (expression : expression) (env : environment) : value =
        | _ -> failwith "not a valid proc call")
   | ExprIf (e1, e2, e3) -> if_eval e1 e2 e3 env
   | ExprAssignment (var, expr) -> assign_eval var expr
-  | ExprLet (_, _)
-  | ExprLetStar (_, _)
+  | ExprLet (_, _) -> failwith "not finished"
+  | ExprLetStar (let_bindlst, exprs) -> let_star let_bindlst exprs
   | ExprLetRec (_, _)     ->
      failwith "Ahahaha!  That is classic Rower."
   | _ -> failwith "no match"
